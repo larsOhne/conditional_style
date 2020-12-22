e2ae40c13db4dd4c75eb4a1e9df844e5963040fb
Lars Ohnemus <ohnemus.lars@gmail.com>
2020-12-22 11:11:06 +0100

    [FEATURE] Single style working properly

--- action.php
+++ action.php
@@ -13,6 +13,7 @@ if (!defined('DOKU_INC')) {
 
 class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
 {
+    protected $search_configs = [];
 
     /**
      * Registers a callback function for a given event
@@ -24,8 +25,8 @@ class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
     public function register(Doku_Event_Handler $controller)
     {
         $controller->register_hook('PLUGIN_STRUCT_CONFIGPARSER_UNKNOWNKEY', 'BEFORE', $this, 'handle_plugin_struct_configparser_unknownkey');        
-        $controller->register_hook(' PLUGIN_STRUCT_AGGREGATIONTABLE_RENDERRESULTROW', 'BEFORE', $this, 'handle_plugin_struct_aggregationtable_renderresultrow_before');        
-        $controller->register_hook(' PLUGIN_STRUCT_AGGREGATIONTABLE_RENDERRESULTROW', 'AFTER', $this, 'handle_plugin_struct_aggregationtable_renderresultrow_after');
+        $controller->register_hook('PLUGIN_STRUCT_AGGREGATIONTABLE_RENDERRESULTROW', 'BEFORE', $this, 'handle_plugin_struct_aggregationtable_renderresultrow_before');        
+        $controller->register_hook('PLUGIN_STRUCT_AGGREGATIONTABLE_RENDERRESULTROW', 'AFTER', $this, 'handle_plugin_struct_aggregationtable_renderresultrow_after');
    
     }
 
@@ -55,7 +56,7 @@ class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
         $event->stopPropagation();
 
         // Try to parse value into ternary statement and show error message if it does not work out
-        if(!preg_match('/\s*[a-zA-z]+\s*(=|<|>|!=)\s*[a-zA-z0-9]+\s*\?\s*".+"\s*:\s*.+"/',$val)){
+        if(!preg_match('/\s*[a-zA-z]+\s*.+\s*[a-zA-z0-9]+\s*\?\s*".+"\s*:\s*.+"/',$val)){
             msg("condstyle: $val is not correct", -1);
             return;
         }
@@ -65,8 +66,31 @@ class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
         $styles = preg_split("/\s*\?\s*/",$val,2)[1];
         $style_true = trim(preg_split('/"\s*:\s*"/',$styles)[0],'"');
         $style_false = trim(preg_split('/"\s*:\s*"/',$styles)[1],'"');
+
+        // Parse operator
+        $operator = NULL;
+        $possible_ops = ["=","<",">","!="];
+        foreach ($possible_ops as $k => $op) {
+            if(preg_match("/\s*[a-zA-z]+\s*$op\s*[a-zA-z0-9]+\s*/",$condition)){
+                $operator = $op;
+            }
+        }
+
+        if(!$operator){
+            msg("condstyle: unknown operator ($val)", -1);
+        }
         
-        $data['config'][$key] = [$condition,$style_true,$style_false];
+        // parse column and argument
+        $column = trim(preg_split("/\s*$operator\s*/",$condition)[0]);
+        $argument = trim(preg_split("/\s*$operator\s*/",$condition)[1]);
+
+        // package parsed command into data
+        $data['config'][$key] = array("operator"    => $operator,
+                                      "column"      => $column,
+                                      "argument"    => $argument,
+                                      "style_true"  => $style_true,
+                                      "style_false" => $style_false
+                                     );
     }
 
      /**
@@ -82,6 +106,78 @@ class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
      */
     public function handle_plugin_struct_aggregationtable_renderresultrow_before(Doku_Event $event, $param)
     {
+        // Retrieve mode, renderer and data from this event
+        $mode = $event->data['mode'];
+        $renderer = $event->data['renderer'];
+        $data = $event->data['data'];
+
+        // Check if mode is xhtml otherwise return
+        if ($mode != 'xhtml') return;
+                
+        // Unpack condition and styles
+        $operator = NULL;
+        $column = NULL;
+        $argument = NULL;
+        $style_true = NULL;
+        $style_false = NULL;
+        extract($data['condstyle']);
+
+        // Check if valid data was send, otherwise return
+        if(!$operator) return;
+
+
+        // Query struct database to get full schema info (in case the column used for condition is not displayed)
+        /** @var SearchConfig $searchConfig */
+        $searchConfig = $event->data['searchConfig'];
+        $searchConfig_hash = spl_object_hash($searchConfig);
+        if (!isset($this->search_configs[$searchConfig_hash])) {
+            // Add new Entry for this search configuration
+            $this->search_configs[$searchConfig_hash] = [];
+
+            // Retrieve Column
+            $cond_column = $searchConfig->findColumn($column);
+            //dbg(var_dump($cond_column));
+
+            // Add all columns to be sure that all information was retrieved and execute query
+            $searchConfig->addColumn('*');
+            $result = $searchConfig->execute();
+
+            // Check for each row if the condition matches and add store that information for later use
+            foreach ($result as $rownum => $row) {
+                /** @var Value $value */
+                foreach ($row as $colnum => $value) {
+                    if ($value->getColumn() === $cond_column) {
+                        $row_val = $value->getRawValue();
+                        // check condition
+                        $cond_applies = false;
+                        switch ($operator) {
+                            case '=':
+                                $cond_applies = $row_val == $argument;
+                                break;
+                            case '<':
+                                //TODO not validated
+                                $num_arg = floatval($argument);
+                                $num_row_val = floatval($argument);
+                                $cond_applies = $num_row_val < $num_arg;
+                                break;
+                            default:
+                                msg("condstyle: unknown operator ($operator)", -1);
+                                break;
+                        }
+
+                        // store condition to inject style later
+                        $this->search_configs[$searchConfig_hash][$rownum] = $cond_applies;
+
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        // save row start position
+        $event->data['rowstart']= mb_strlen($renderer->doc);
+        
     }
 
      /**
@@ -97,6 +193,56 @@ class action_plugin_conditionalstyle extends DokuWiki_Action_Plugin
      */
     public function handle_plugin_struct_aggregationtable_renderresultrow_after(Doku_Event $event, $param)
     {
+        // Retrieve and check mode
+        $mode = $event->data['mode'];
+        if ($mode != 'xhtml') return;
+
+        // Retrieve renderer
+        $renderer = $event->data['renderer'];
+
+        // Retrieve searchconfig and data
+        /** @var SearchConfig $searchConfig */
+        $searchConfig = $event->data['searchConfig'];
+        $data = $event->data['data'];
+        $rownum  = $event->data['rownum'];
+        $rowstart = $event->data['rowstart'];
+                
+        // Unpack condition and styles
+        $operator = NULL;
+        $column = NULL;
+        $argument = NULL;
+        $style_true = NULL;
+        $style_false = NULL;
+        extract($data['condstyle']);
+
+        // Check if proper info is available
+        if (!isset($style_true)) return;
+        if (!isset($style_false)) return;
+        if (!$rowstart) return;
+        
+
+        // Lookup the style condition
+        $searchConfig_hash = spl_object_hash($searchConfig);
+        $cond_applies = $this->search_configs[$searchConfig_hash][$rownum];
+        if (!isset($cond_applies)) return;
+
+        // set the style for this column based on condition
+        $style = $cond_applies ? $style_true : $style_false;
+        
+        // split doc to inject styling
+        $rest = mb_substr($renderer->doc, 0,  $rowstart);
+        $row = mb_substr($renderer->doc, $rowstart);
+        $row = ltrim($row);
+        //check if we processing row
+        if (mb_substr($row, 0, 3) != '<tr') return;
+
+        $tr_tag = mb_substr($row, 0, 3);
+        $tr_rest = mb_substr($row, 3);
+
+        if(trim($style) != "")
+            $renderer->doc = $rest . $tr_tag . ' style="'.$style.'" ' . $tr_rest;
+
+
     }
 
 }
