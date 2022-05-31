bb394d70c43c7a1343a550b679b2c41232588b00
Lars Ohnemus <ohnemus.lars@gmail.com>
2022-05-31 12:04:31 +0200

    [FIX] Moved functions out of global namespace

--- meta/Operator.php
+++ meta/Operator.php
@@ -12,12 +12,12 @@ namespace dokuwiki\plugin\structcondstyle\meta;
 class Operator
 {
     private $reg;
-    private $eval_func_name;
+    private $eval_func;
 
-    function __construct($reg,$eval_func)
+    function __construct($reg, $eval_func)
     {
         $this->reg = $reg;
-        $this->eval_func_name = $eval_func;
+        $this->eval_func = $eval_func;
     }
 
     function getReg(){
@@ -25,8 +25,43 @@ class Operator
     }
 
     function evaluate($lhs, $rhs){
-        return call_user_func($this->eval_func_name,$lhs,$rhs);
+        // Evaluate the operator
+        $eval = $this->eval_func;
+        return $eval($lhs,$rhs);
     }
+}
+
+class NumericOperator extends Operator
+{
+
+    private function make_numeric($value){
+        if(is_numeric($value))
+            return floatval($value);
+        if(is_string($value)){
+            if($value == "now")
+                return time();
+            else
+                return strtotime($value);
+            
+        }
+        else
+            return FALSE;
+    }
+
+    function evaluate($lhs, $rhs)
+    {
+        // First convert both sides to their numerical values
+        $lvalue = $this->make_numeric($lhs);
+        $rvalue = $this->make_numeric($rhs);
+
+        // If value could not be parsed, return false
+        if($lvalue === FALSE or $rvalue === FALSE)
+            return false;
+
+        // otherwise simply return the super function 
+        return parent::evaluate($lvalue, $rvalue);
+    }
+
 }
 
 ?>
